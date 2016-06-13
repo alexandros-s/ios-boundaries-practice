1c237e80e5f8f8c928b29fcfaadb207be53336d2
alexandros <a.spyropoulos@quidco.com>
2016-06-13 15:23:01 +0100

    Initial Commit

---
+++ BoundariesInPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoundariesInPractice
+//
+//  Created by Alexandros Spyropoulos on 13/06/2016.
+//  Copyright © 2016 Alexandros Spyropoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
